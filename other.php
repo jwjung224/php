c5b2dea64c74a9143b73d94ce8a2e82a3d1cdd26
Joon <jwjung224@gmail.com>
2017-02-21 03:38:14 -0800

    Update other.php

--- other.php
+++ other.php
@@ -3,14 +3,15 @@
 // finoacci number sequence
 // recursion required
 function fibo($num){
-	if($num <= 1){
-	    return $num;
-	}
-	else{
-	    return fibo($num - 1) + fibo($num - 2);
-	}
+    if($num <= 1){
+	return $num;
+    }
+    else{
+	return fibo($num - 1) + fibo($num - 2);
+    }
 }
 
+// helper function used in bubbleSort
 function swap(&$arr,$p1,$p2)
 {
     $temp = $arr[$p2];
@@ -20,16 +21,14 @@ function swap(&$arr,$p1,$p2)
 
 // bubble sort in PHP
 function bubbleSort($arr){
-	$count = count($arr);
-	for($i = 1; $i < $count; $i++){
-		$first = $arr[0];
-	    for ($j = 1; $j < ($count - $i + 1); $j++){        
+    $count = count($arr);
+    for($i = 1; $i < $count; $i++){
+	$first = $arr[0];
+	for ($j = 1; $j < ($count - $i + 1); $j++){        
             if ($arr[$j-1] > $arr[$j])
-            {
                 swap($arr, $j-1, $j);
-            } 
-	    }
 	}
-	return $arr;
+    }
+    return $arr;
 }
 ?>
